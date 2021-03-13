587c4d0601a17dc08518498272b61ee8c79773e5
jchrys <jchrys@me.com>
2021-03-13 19:02:05 +0900

    add PhoneNumber Regex

--- Contacts/task/src/contacts/util/PhoneNumberValidator.java
+++ Contacts/task/src/contacts/util/PhoneNumberValidator.java
@@ -1,11 +1,18 @@
 package contacts.util;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhoneNumberValidator {
     private PhoneNumberValidator() {
     }
 
+    private static String regex = "[+]?([a-zA-Z0-9]+[- ]([a-zA-Z0-9]){2,}|\\([a-zA-Z0-9]+\\)[- ]([a-zA-Z0-9]){2,}|([a-zA-Z0-9])+[- ]\\(([a-zA-Z0-9]){2,}\\))([ -][a-zA-Z0-9]{2,})*";
+    private static Pattern pattern = Pattern.compile(regex);
+
     public static boolean isValid(String number) {
-        return false;
+        Matcher matcher = pattern.matcher(number);
+        return matcher.matches();
     }
 }
